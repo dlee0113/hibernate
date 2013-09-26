d950e8960c949ba56f0043364c35fcebf00a3e12
Dong Lee <dlee0113@gmail.com>
2013-09-26 08:19:37 -0400

    added select HQL

--- hibernate_one_to_one_annotation/src/main/java/com/mkyong/App.java
+++ hibernate_one_to_one_annotation/src/main/java/com/mkyong/App.java
@@ -1,7 +1,9 @@
 package com.mkyong;
 
 import java.util.Date;
+import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
 import com.mkyong.stock.Stock;
@@ -9,35 +11,49 @@ import com.mkyong.stock.StockDetail;
 import com.mkyong.util.HibernateUtil;
 
 public class App {
+	private static Logger log = Logger.getLogger(App.class);
+	
 	public static void main(String[] args) {
-		System.out.println("Hibernate one to one (Annotation)");
+		log.info("Hibernate one to one (Annotation)");
+		
 		Session session = HibernateUtil.getSessionFactory().openSession();
 
 		session.beginTransaction();
 
 		Stock stock = new Stock();
-
 		stock.setStockId(HibernateUtil.getNext(session));
 		stock.setStockCode("100");
 		stock.setStockName("GOOG");
 		
-		System.out.println(stock);
+		log.info("stock - " + stock);
 
 		StockDetail stockDetail = new StockDetail();
 		stockDetail.setCompName("PADINI Holding Malaysia");
 		stockDetail.setCompDesc("one stop shopping");
 		stockDetail.setRemark("vinci vinci");
 		stockDetail.setListedDate(new Date());
-		
 		stock.setStockDetail(stockDetail);
 		stockDetail.setStock(stock);
 
-		System.out.println(stockDetail);
-		System.out.println(stock);
+		log.info("stockDetail - " + stockDetail);
 		
 		session.save(stock);
 		session.getTransaction().commit();
+		
+		session.beginTransaction();
 
-		System.out.println("Done");
+		List<Stock> stockList = session.createQuery("from Stock").list();
+		for (Stock s: stockList) {
+			log.info("stock - " + s);
+		}
+		
+		List<StockDetail> stockDetailList =  session.createQuery("from StockDetail").list();
+		for (StockDetail sd: stockDetailList) {
+			log.info("stockDetail - " + sd);
+		}
+		
+		session.close();
+		
+		log.info("Done");
 	}
 }
